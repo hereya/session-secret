88472efdd36162a338a0f9152f2ea4663a17ac5d
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-07-09 15:14:27 +0200

    Initial commit
    Added the following files:
    - .gitignore
    - README.md
    - hereyarc.yaml
    - main.tf

---
+++ README.md
@@ -0,0 +1,4 @@
+# SESSION SECRET
+
+DReAM package for generating key for session data symmetric signing.
+Secret key is stored in AWS parameter store.
---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+
+  required_providers {
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "~> 3.4"
+    }
+
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "random" {}
+provider "aws" {}
+
+resource "random_pet" "namespace" {}
+
+resource "random_password" "secret" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "secret" {
+  name        = "/session_secret/v2/${random_pet.namespace.id}"
+  description = "Session secret value"
+  type        = "SecureString"
+  value       = random_password.secret.result
+}
+
+data "aws_region" "current" {}
+
+output "sessionSecret" {
+  value = {
+    type   = "ssm"
+    key    = aws_ssm_parameter.secret.name
+    region = data.aws_region.current.name
+    arn    = aws_ssm_parameter.secret.arn
+  }
+}
+
